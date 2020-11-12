2768cfd7160a617d3dbbc546596d24f2e5b3c2da
Hayder Abidi <hayder.abidi@student.rocva.nl>
2020-11-12 12:16:53 +0100

    datatypes

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,10 @@
+let mijnnVoornaam = "Hayder";
+let mijnAchternaam = "Abidi";
+let leeftijd = "17"
+let ikBenStemGerechtig = false;
+let ikMagLegaalAlcoholKopen = false;
+let naamStudent = "Hayder";
+let ikMagBrommerRijden = false;
+let softwareOntwikkeling = true;
+
+console.log(naamStudent)
