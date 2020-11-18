0b24b15baf95c317659ed9c06def13369751b50d
Hayder Abidi <hayder.abidi@student.rocva.nl>
2020-11-18 11:38:41 +0100

    stringplak

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,5 @@
+let mijnVoornaam = "Hayder";
+let mijnAchternaam = "Abidi";
+let mijnLeeftijd = "17";
+
+let volledigeZin = "Mijn naam is " + mijnVoornaam + " " + mijnAchternaam + " en mijn leeftijd is " + mijnLeeftijd + " jaar"
