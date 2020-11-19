f3160c53b2aade36541ec92126603db8d8e448d1
Hayder Abidi <hayder.abidi@student.rocva.nl>
2020-11-19 12:20:08 +0100

    dom intelligent

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -5,5 +5,19 @@ console.log(window.document);
 
 // document.querySelector("#posterimg1").src = null;
 
-
 // document.querySelector("#poster1");
+var elem = document.querySelector('#poster2');
+
+elem.style.backgroundColor = 'blue';
+
+
+// document.querySelector("#poster");
+
+var elem = document.querySelector('#poster1');
+
+
+
+// Set a background color
+
+elem.style.backgroundColor = 'yellow';
+
